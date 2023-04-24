fe6612f08d4c2037d9cd69ab5607f4a378364629
mib <mib@kanp.ai>
2023-04-24 11:16:30 +0200

    neovim: retab

--- programs/neovim/config.lua
+++ programs/neovim/config.lua
@@ -21,9 +21,9 @@ function cfg_lspconfig()
 	local lsp_defaults = lspconfig.util.default_config
 
 	lsp_defaults.capabilities = vim.tbl_deep_extend(
-	  'force',
-	  lsp_defaults.capabilities,
-	  require('cmp_nvim_lsp').default_capabilities()
+		'force',
+		lsp_defaults.capabilities,
+		require('cmp_nvim_lsp').default_capabilities()
 	)
 end
 
@@ -38,35 +38,35 @@ function cfg_nvimcmp()
 	local select_opts = {behavior = cmp.SelectBehavior.Select}
 
 	cmp.setup({
-	  snippet = {
+		snippet = {
 		expand = function(args)
-		  luasnip.lsp_expand(args.body)
+			luasnip.lsp_expand(args.body)
 		end
-	  },
-	  sources = {
+		},
+		sources = {
 		{name = 'path'},
 		{name = 'nvim_lsp', keyword_length = 1},
 		{name = 'buffer', keyword_length = 3},
 		{name = 'luasnip', keyword_length = 2},
-	  },
-	  window = {
+		},
+		window = {
 		documentation = cmp.config.window.bordered()
-	  },
-	  formatting = {
+		},
+		formatting = {
 		fields = {'menu', 'abbr', 'kind'},
 		format = function(entry, item)
-		  local menu_icon = {
+			local menu_icon = {
 			nvim_lsp = 'λ',
 			luasnip = '⋗',
 			buffer = 'Ω',
 			path = '🖫',
-		  }
+			}
 
-		  item.menu = menu_icon[entry.source.name]
-		  return item
+			item.menu = menu_icon[entry.source.name]
+			return item
 		end,
-	  },
-	  mapping = {
+		},
+		mapping = {
 		['<Up>'] = cmp.mapping.select_prev_item(select_opts),
 		['<Down>'] = cmp.mapping.select_next_item(select_opts),
 
@@ -81,41 +81,41 @@ function cfg_nvimcmp()
 		['<CR>'] = cmp.mapping.confirm({select = false}),
 
 		['<C-f>'] = cmp.mapping(function(fallback)
-		  if luasnip.jumpable(1) then
+			if luasnip.jumpable(1) then
 			luasnip.jump(1)
-		  else
+			else
 			fallback()
-		  end
+			end
 		end, {'i', 's'}),
 
 		['<C-b>'] = cmp.mapping(function(fallback)
-		  if luasnip.jumpable(-1) then
+			if luasnip.jumpable(-1) then
 			luasnip.jump(-1)
-		  else
+			else
 			fallback()
-		  end
+			end
 		end, {'i', 's'}),
 
 		['<Tab>'] = cmp.mapping(function(fallback)
-		  local col = vim.fn.col('.') - 1
+			local col = vim.fn.col('.') - 1
 
-		  if cmp.visible() then
+			if cmp.visible() then
 			cmp.select_next_item(select_opts)
-		  elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+			elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
 			fallback()
-		  else
+			else
 			cmp.complete()
-		  end
+			end
 		end, {'i', 's'}),
 
 		['<S-Tab>'] = cmp.mapping(function(fallback)
-		  if cmp.visible() then
+			if cmp.visible() then
 			cmp.select_prev_item(select_opts)
-		  else
+			else
 			fallback()
-		  end
+			end
 		end, {'i', 's'}),
-	  },
+		},
 	})
 end
 
