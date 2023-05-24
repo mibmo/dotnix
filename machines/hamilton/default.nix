d8503ce11fd8b6c7ad261bf2a00a9dea556e0705
mib <mib@kanp.ai>
2023-05-24 17:16:11 +0200

    chore(machine): remove deprecated grub option

--- machines/hamilton/default.nix
+++ machines/hamilton/default.nix
@@ -7,7 +7,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub = {
     enable = true;
-    version = 2;
     device = "nodev";
     efiSupport = true;
     enableCryptodisk = true;
