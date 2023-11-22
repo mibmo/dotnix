d721e91535a7d3c1c5b01a6105e07492af5cff18
mib <mib@mib.dev>
2023-11-22 02:48:40 +0100

    fix(roles/productivity): reenable freecad since it's no longer borked
    freecad was broken on unstable due to pyside2 not supporting nixpkgs' current
    python latest version which was > 3.10. This has since been fixed, so
    freecad works again

--- roles/productivity/default.nix
+++ roles/productivity/default.nix
@@ -1,7 +1,7 @@
 { pkgs, settings, ... }: {
   home-manager.users.${settings.user.name}.home.packages = with pkgs; [
     blender
-    #freecad # broken on unstable as of 2023-11-02 due to pyside2 not supporting python > 3.10
+    freecad
     kicad
     solvespace
     inkscape
