b591fc0d1c6ff67c24f281df42b560f27c24d2b1
mib <mib@mib.dev>
2024-02-26 22:00:52 +0100

    roles/networking: automatic login to public access points
    hopefully template is generic enough to support many different aps,
    but if not then thankfully it's easy to change. :)

--- roles/networking/default.nix
+++ roles/networking/default.nix
@@ -1,4 +1,4 @@
-{ settings, ... }: {
+{ lib, pkgs, settings, ... }: {
   imports = [
     ./vpn.nix
   ];
@@ -12,6 +12,38 @@
         "149.112.112.112"
         "2620:fe::fe"
         "2620:fe::9"
+      ];
+      dispatcherScripts = map
+        ({ name, ap-name, effect }: {
+          type = "basic";
+          source = pkgs.writeShellScript "testHook" ''
+            log() {
+              logger "[${name}]" "$@"
+            }
+
+            if [ "$2" = "up" ] && [ "$CONNECTION_ID" = "${ap-name}" ]; then
+              log "attempting login to $CONNECTION_ID via $IP4_GATEWAY"
+
+              if (${effect});
+                then log "login successful"
+                else log "login failed"
+              fi
+            fi
+          '';
+        }) [
+        {
+          name = "dsbwifi";
+          ap-name = ".DSB_Wi-Fi";
+          effect = ''
+            ${lib.getExe pkgs.curl} \
+                --silent \
+                --insecure \
+                --connect-timeout 2 \
+                --header 'Host: dsbwifi.dk' \
+                --form 'language=dk' \
+                "https://$IP4_GATEWAY/login.php"
+          '';
+        }
       ];
     };
   };
