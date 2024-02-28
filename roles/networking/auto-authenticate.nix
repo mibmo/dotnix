8dfe3b3a1e6416e2c037ff7066f7219d91c063bf
mib <mib@mib.dev>
2024-02-28 13:35:44 +0100

    roles/networking: split network authentication dispatchers into own file

---
+++ roles/networking/auto-authenticate.nix
@@ -0,0 +1,34 @@
+{ lib, pkgs, ... }: {
+  networking.networkmanager.dispatcherScripts = map
+    ({ name, ap-name, effect }: {
+      type = "basic";
+      source = pkgs.writeShellScript "testHook" ''
+        log() {
+          logger "[${name}]" "$@"
+        }
+
+        if [ "$2" = "up" ] && [ "$CONNECTION_ID" = "${ap-name}" ]; then
+          log "attempting login to $CONNECTION_ID via $IP4_GATEWAY"
+
+          if (${effect});
+            then log "login successful"
+            else log "login failed"
+          fi
+        fi
+      '';
+    }) [
+    {
+      name = "dsbwifi";
+      ap-name = ".DSB_Wi-Fi";
+      effect = ''
+        ${lib.getExe pkgs.curl} \
+            --silent \
+            --insecure \
+            --connect-timeout 2 \
+            --header 'Host: dsbwifi.dk' \
+            --form 'language=dk' \
+            "https://$IP4_GATEWAY/login.php"
+      '';
+    }
+  ];
+}
