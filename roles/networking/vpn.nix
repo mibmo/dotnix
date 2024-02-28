f4f16de88a8ba67aeecb759ca2695acf1f197eb9
mib <mib@mib.dev>
2024-02-28 13:29:50 +0100

    roles/networking/vpn: dont let tailscale overwrite resolv.conf

--- roles/networking/vpn.nix
+++ roles/networking/vpn.nix
@@ -2,6 +2,9 @@
   services.tailscale = {
     enable = true;
     openFirewall = true;
-    extraUpFlags = [ "--login-server=https://headscale.kanp.ai" ];
+    extraUpFlags = [
+      "--login-server=https://headscale.kanp.ai"
+      "--accept-dns=false"
+    ];
   };
 }
