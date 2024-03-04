8037b8c5fbf3b9243d3d0c4923e2f53d7b6f66f0
mib <mib@mib.dev>
2024-03-04 17:13:35 +0100

    roles/internet/syncthing: version secret staggered
    and move to function to DRY

--- roles/internet/syncthing/folders.nix
+++ roles/internet/syncthing/folders.nix
@@ -1,21 +1,31 @@
-{ home }: {
+{ home }:
+let
+  staggered =
+    { days ? 14 # every two weeks
+    , clean ? 3600 # every hour
+    }: {
+      type = "staggered";
+      params = {
+        cleanInterval = toString clean;
+        maxAge = toString (days * 86400);
+      };
+    };
+in
+{
   "Secret" = {
     id = "0hhw6-hojo4";
     devices = [ "mobai" "ichi" ];
     path = "${home}/.secret";
+    versioning = staggered {
+      days = 7 * 4 * 2; # two months
+    };
     enable = true;
   };
   "Code" = {
     id = "tcvau-dajz7";
     devices = [ "mobai" "ichi" ];
     path = "${home}/dev";
-    versioning = {
-      type = "staggered";
-      params = {
-        cleanInterval = "3600"; # every hour
-        maxAge = "1209600"; # two weeks
-      };
-    };
+    versioning = staggered { };
     enable = true;
   };
   "Books" = {
