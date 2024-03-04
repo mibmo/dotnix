1399f943b3970d7fe4a74ddcc514caacce53a10f
mib <mib@mib.dev>
2024-03-04 17:07:39 +0100

    roles/internet/syncthing: sync code folder

--- roles/internet/syncthing/folders.nix
+++ roles/internet/syncthing/folders.nix
@@ -5,6 +5,19 @@
     path = "${home}/.secret";
     enable = true;
   };
+  "Code" = {
+    id = "tcvau-dajz7";
+    devices = [ "mobai" "ichi" ];
+    path = "${home}/dev";
+    versioning = {
+      type = "staggered";
+      params = {
+        cleanInterval = "3600"; # every hour
+        maxAge = "1209600"; # two weeks
+      };
+    };
+    enable = true;
+  };
   "Books" = {
     id = "6whpv-fec6p";
     devices = [ "mobai" "holger" ];
