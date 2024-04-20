54ebea0b5bcaf90276d9f53ae665d1c4cd169e59
mib <mib@mib.dev>
2024-04-20 22:59:36 +0200

    modules/zfs: auto snapshotting

--- modules/zfs/default.nix
+++ modules/zfs/default.nix
@@ -1,5 +1,14 @@
-{ ... }: {
+{ lib, ... }: {
   services.zfs = {
+    autoSnapshot = {
+      enable = true;
+      frequent = lib.mkDefault 4;
+      hourly = lib.mkDefault 24;
+      daily = lib.mkDefault 7;
+      weekly = lib.mkDefault 4;
+      monthly = lib.mkDefault 12;
+    };
+
     autoScrub = {
       enable = true;
       interval = "daily";
