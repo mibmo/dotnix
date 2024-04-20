e29a020308011c62a0c73710823d5100096c36bd
mib <mib@mib.dev>
2024-04-20 14:56:47 +0200

    roles/desktop/communication: add fluffychat

--- roles/desktop/communication/default.nix
+++ roles/desktop/communication/default.nix
@@ -2,12 +2,14 @@
   home.packages = with pkgs; [
     cinny-desktop
     element-desktop
+    fluffychat
     nheko
   ];
 
   persist.user.directories = [
     ".config/Element"
     ".config/nheko"
+    ".local/share/chat.fluffy.fluffychat"
     ".local/share/cinny"
   ];
 }
