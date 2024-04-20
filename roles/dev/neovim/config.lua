394b68944bcea9cdcdc5d5e3dd1bbb4c1fe6ba9b
mib <mib@mib.dev>
2024-04-20 20:27:57 +0200

    roles/dev/neovim: update to luasnip v2

--- roles/dev/neovim/config.lua
+++ roles/dev/neovim/config.lua
@@ -499,7 +499,7 @@ require("lazy").setup({
 			"hrsh7th/cmp-buffer",
 			{
 				"L3MON4D3/LuaSnip",
-				version = "1.*",
+				version = "v2.*",
 				build = "make install_jsregexp",
 				config = Cfg_luasnip,
 				dependencies = {
