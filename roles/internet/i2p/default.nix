31ab8f14945eb67c8c76b4949aa5df4fe0ea721a
mib <mib@mib.dev>
2024-04-20 12:27:09 +0200

    roles/internet/i2p: hardcode some reseed urls

--- roles/internet/i2p/default.nix
+++ roles/internet/i2p/default.nix
@@ -3,5 +3,10 @@
     enable = true;
     yggdrasil.enable = true;
     proto.httpProxy.enable = true;
+    reseed.urls = [
+      "reseed2.i2p.net"
+      "i2phides.me"
+      "www2.mk16.de"
+    ];
   };
 }
