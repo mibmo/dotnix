4625195d6ee7b49b486587cd91625f1ee4b0dc10
mib <mib@mib.dev>
2024-05-26 23:57:43 +0200

    modules/nix: garbage collect only when needed

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -8,18 +8,31 @@
       options = "--delete-older-than 7d";
     };
 
+    optimise = {
+      automatic = true;
+      dates = [ "hourly" ];
+    };
+
     # pin local nixpkgs to flake nixpkgs
     registry.nixpkgs.flake = inputs.nixpkgs;
 
-    settings = {
-      # enable flakes
-      experimental-features = [ "nix-command" "flakes" ];
+    settings =
+      let
+        KiB = 1024;
+        MiB = 1024 * KiB;
+        GiB = 1024 * MiB;
+      in
+      {
+        # enable flakes
+        experimental-features = [ "nix-command" "flakes" ];
 
-      auto-optimise-store = true;
+        # gc
+        min-free = 1 * GiB;
+        max-free = 5 * GiB;
 
-      # avoid unwanted gc when using flakes
-      keep-outputs = true;
-      keep-derivations = true;
-    };
+        # avoid unwanted gc when using flakes
+        keep-outputs = true;
+        keep-derivations = true;
+      };
   };
 }
