9d8736fb35c6ef0642dd0fb4f017e1152add77ca
mib <mib@mib.dev>
2024-06-07 22:57:54 +0200

    modules/substituters: flexible definitions; attrsets too

--- modules/substituters/default.nix
+++ modules/substituters/default.nix
@@ -1,25 +1,42 @@
 { lib, config, ... }: {
   options.substituters = lib.mkOption {
-    type = with lib.types; listOf str;
+    type = with lib.types; listOf (oneOf [ str attrs ]);
     default = [ ];
     example = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
   };
 
   config.nix.settings =
     let
-      substituters = map extractSubstituter config.substituters;
-      extractSubstituter = key:
-        let
-          parts = lib.strings.splitString "-" key;
-          url = "https://" +
-            lib.strings.concatStringsSep
-              "-"
-              (lib.lists.take (lib.lists.length parts - 1) parts);
-        in
-        { inherit key url; };
+      substituters =
+        map
+          (s:
+            lib.attrsets.filterAttrs
+              (_: v: v != null)
+              (if builtins.isAttrs s
+              then s
+              else
+                let
+                  inherit (builtins) elemAt match;
+                  inherit (lib.lists) reverseList;
+                  inherit (lib.strings) concatStrings stringToCharacters;
+
+                  reverse = text: concatStrings (reverseList (stringToCharacters text));
+
+                  matches = match "(([a-z]+)://)?(.*):([a-zA-Z0-9+/=]+)" s;
+
+                  hostParts = match "(([0-9]+)-)?(.*)" (reverse (elemAt matches 2));
+
+                  protocol = elemAt matches 1;
+                  host = reverse (elemAt hostParts 2);
+                  suffix = reverse (elemAt hostParts 1);
+                  key = elemAt matches 3;
+                in
+                { inherit protocol host suffix key; })
+          )
+          config.substituters;
     in
     {
-      substituters = map (s: s.url) substituters;
-      trusted-public-keys = map (s: s.key) substituters;
+      substituters = map (s: "${s.protocol or "https"}://${s.host}") substituters;
+      trusted-public-keys = map (s: "${s.host}${lib.optionalString (s ? "suffix") "-${s.suffix}"}:${s.key}") substituters;
     };
 }
