2d889c0cb22ae3293fcf791c382f6918976d7d83
mib <mib@kanp.ai>
2024-07-06 01:56:07 +0200

    config: enable ipfs for macadamia and sakamoto

--- config.nix
+++ config.nix
@@ -33,6 +33,7 @@ let
         ./roles/desktop/gnome
         ./roles/internet/thunderbird
         ./roles/internet/syncthing
+        ./roles/internet/ipfs
       ];
       keys.ssh = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPnTI64CyDnCoQFZKWI0T1Z6YcyVW8XrJypWxD9HdMGW" ];
     };
@@ -46,6 +47,7 @@ let
         ./roles/dev
 
         ./roles/desktop/gnome
+        ./roles/internet/ipfs
       ];
       keys.ssh = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBPQ3PpMq59iLXy5j/r/mwTlrZsU2xhyDtTXdHv6yM7b" ];
     };
