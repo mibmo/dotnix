bfedf55e41e5f7e09a14a82a28c61c53b2fc2d32
mib <mib@kanp.ai>
2024-07-30 15:34:47 +0200

    roles/gaming/games: persist proton data
    for space engineers specifically

--- roles/gaming/games.nix
+++ roles/gaming/games.nix
@@ -2,6 +2,7 @@
 
 {
   persist.user.directories = [
+    ".local/share/Steam/steamapps/compatdata" # proton stuff (usually also saves)
     ".local/share/Terraria"
     ".local/share/binding of isaac afterbirth+ mods"
     ".local/share/binding of isaac afterbirth+"
