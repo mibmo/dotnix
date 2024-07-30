8d63d33674404ed145ab06377c43e3df02015545
mib <mib@kanp.ai>
2024-07-30 16:17:37 +0200

    flake: comment unused nixpkgs releases
    should significantly improve eval time

--- flake.nix
+++ flake.nix
@@ -5,12 +5,12 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
     nixpkgs-24_05.url = "nixpkgs/nixos-24.05";
-    nixpkgs-23_11.url = "nixpkgs/nixos-23.11";
-    nixpkgs-23_05.url = "nixpkgs/nixos-23.05";
-    nixpkgs-22_11.url = "nixpkgs/nixos-22.11";
-    nixpkgs-22_05.url = "nixpkgs/nixos-22.05";
-    nixpkgs-21_11.url = "nixpkgs/nixos-21.11";
-    nixpkgs-21_05.url = "nixpkgs/nixos-21.05";
+    #nixpkgs-23_11.url = "nixpkgs/nixos-23.11";
+    #nixpkgs-23_05.url = "nixpkgs/nixos-23.05";
+    #nixpkgs-22_11.url = "nixpkgs/nixos-22.11";
+    #nixpkgs-22_05.url = "nixpkgs/nixos-22.05";
+    #nixpkgs-21_11.url = "nixpkgs/nixos-21.11";
+    #nixpkgs-21_05.url = "nixpkgs/nixos-21.05";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nur.url = "github:nix-community/NUR";
