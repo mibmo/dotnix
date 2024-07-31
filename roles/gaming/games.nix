c08c0fbd76208c6801cf952c9d7801a9f44f3e4f
mib <mib@kanp.ai>
2024-07-31 10:25:19 +0200

    roles/gaming/games: persist factorio data

--- roles/gaming/games.nix
+++ roles/gaming/games.nix
@@ -2,6 +2,7 @@
 
 {
   persist.user.directories = [
+    ".factorio"
     ".local/share/Steam/steamapps/compatdata" # proton stuff (usually also saves)
     ".local/share/Terraria"
     ".local/share/binding of isaac afterbirth+ mods"
