d7db452b18fc4229a960f6a2010a4c2183d83cb2
mib <mib@kanp.ai>
2024-08-04 20:32:10 +0200

    roles/gaming/games: compatdata was already persisted
    my bad.

--- roles/gaming/games.nix
+++ roles/gaming/games.nix
@@ -3,7 +3,6 @@
 {
   persist.user.directories = [
     ".factorio"
-    ".local/share/Steam/steamapps/compatdata" # proton stuff (usually also saves)
     ".local/share/Terraria"
     ".local/share/binding of isaac afterbirth+ mods"
     ".local/share/binding of isaac afterbirth+"
