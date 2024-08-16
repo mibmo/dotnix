5bfa56628cfd5e635f0aa76aed5e9f230cf11df3
mib <mib@kanp.ai>
2024-08-16 15:11:13 +0200

    roles/desktop/notes: need to use upstream electron to work

--- roles/desktop/notes/default.nix
+++ roles/desktop/notes/default.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 let
   logseq = pkgs.logseq.override {
-    # graph might not sync properly
-    electron = pkgs.electron_29;
+    # graph doesnt sync properly; needs upstream major version (electron 27)
+    #electron = pkgs.electron_29;
   };
 in
 {
