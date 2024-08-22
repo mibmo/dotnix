851e6752e0ba29796884cfcfe787f1ef95432cf0
mib <mib@kanp.ai>
2024-08-22 21:00:47 +0200

    roles/desktop/notes: allow global and per-graph config for logseq

--- roles/desktop/notes/default.nix
+++ roles/desktop/notes/default.nix
@@ -4,9 +4,28 @@ let
     # graph doesnt sync properly; needs upstream major version (electron 27)
     #electron = pkgs.electron_29;
   };
+
+  # path to logseq graphs relative to home
+  graphs = [
+    "assets/diary"
+    "assets/notes"
+  ];
 in
 {
-  home.packages = [ logseq ];
+  home = {
+    packages = [ logseq ];
+    settings.home.file =
+      { ".config/Logseq/configs.edn".source = ./global.edn; } //
+      (builtins.listToAttrs
+        (
+          map
+            (path: {
+              name = "${path}/logseq/config.edn";
+              value.source = ./graph.edn;
+            })
+            graphs
+        ));
+  };
 
   persist.user.directories = [
     ".config/Logseq"
