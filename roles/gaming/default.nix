2e8f5e0d6b4fb2b13b1e529d0a7eb5c861884af1
mib <mib@kanp.ai>
2024-09-08 15:45:25 +0200

    roles/gaming: remove options unique to hamilton host

--- roles/gaming/default.nix
+++ roles/gaming/default.nix
@@ -13,14 +13,7 @@
 
     gamemode = {
       enable = true;
-      settings = {
-        general.renice = 10;
-        gpu = {
-          apply_gpu_optimisations = "accept-responsibility";
-          gpu_device = 0;
-          amd_performance_level = "auto";
-        };
-      };
+      settings.general.renice = 10;
     };
 
     gamescope = {
