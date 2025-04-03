a277c90196dadb8418e7e172e176f705f7cba57d
mib <mib@kanp.ai>
2025-04-04 01:42:02 +0200

    roles/internet/tor: open control port and add nyx package

--- roles/internet/tor/default.nix
+++ roles/internet/tor/default.nix
@@ -1,7 +1,10 @@
-{ ... }:
+{ pkgs-23_11, ... }:
 {
   services.tor = {
     enable = true;
     client.enable = true;
+    settings.ControlPort = [ { port = 9051; } ];
   };
+
+  home.packages = [ pkgs-23_11.nyx ];
 }
