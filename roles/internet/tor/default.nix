3b63d45f7c74848959cac80f059cdc7336d750c9
mib <mib@kanp.ai>
2025-04-04 01:42:31 +0200

    roles/internet/tor: allow client to use ipv6

--- roles/internet/tor/default.nix
+++ roles/internet/tor/default.nix
@@ -3,7 +3,10 @@
   services.tor = {
     enable = true;
     client.enable = true;
-    settings.ControlPort = [ { port = 9051; } ];
+    settings = {
+      ControlPort = [ { port = 9051; } ];
+      ClientUseIPv6 = true;
+    };
   };
 
   home.packages = [ pkgs-23_11.nyx ];
