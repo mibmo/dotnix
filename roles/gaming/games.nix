763645c156b82146ff66a809518c1cda2c150969
mib <mib@kanp.ai>
2025-09-15 03:34:20 +0200

    add ftl game & it's mod launcher

--- roles/gaming/games.nix
+++ roles/gaming/games.nix
@@ -3,17 +3,20 @@
 {
   persist.user.directories = [
     ".factorio"
+    ".local/share/FasterThanLight"
     ".local/share/PrismLauncher"
     ".local/share/Terraria"
     ".local/share/binding of isaac afterbirth+ mods"
     ".local/share/binding of isaac afterbirth+"
     ".local/share/etterna"
     ".local/share/osu"
+    ".local/share/slipstream"
   ];
 
   home.packages = with pkgs; [
     etterna
     osu-lazer-bin
     prismlauncher
+    slipstream
   ];
 }
