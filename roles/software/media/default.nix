3c21cb0e1c4cbd6f6c902339ada3f3a84b119186
mib <mib@kanp.ai>
2026-01-19 16:37:22 +0100

    add picard as media software

--- roles/software/media/default.nix
+++ roles/software/media/default.nix
@@ -1,6 +1,17 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [ feishin ];
+  home.packages = with pkgs; [
+    feishin
+    picard
+  ];
 
-  persist.user.directories = [ ".config/feishin" ];
+  persist.user = {
+    directories = [
+      ".config/MusicBrainz/Picard"
+      ".config/feishin"
+    ];
+    files = [
+      ".config/MusicBrainz/Picard.ini"
+    ];
+  };
 }
